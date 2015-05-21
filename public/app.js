74016c7a4e7675cd312ec669ff47f4684afb1458
Samuel Leavin <sam.leavin@att.net>
2015-05-21 16:48:09 -0400

    working and sliding.

--- public/app.js
+++ public/app.js
@@ -1,22 +1,63 @@
 var app = angular.module("FlashCards", [])
 
 app.controller("FlashCardController", function ($scope) {
-	$scope.flashCard = {
-	    question: 'What is Angular?',
-	    answers: [
-	        { text: 'A front-end framework for great power!', correct: true },
-	        { text: 'Something lame, who cares, whatever.', correct: false },
-	        { text: 'Some kind of fish, right?', correct: false }
-	    ]
-	};
+	// $scope.flashCard = {
+	//     question: 'What is Angular?',
+	//     answers: [
+	//         { text: 'A front-end framework for great power!', correct: true },
+	//         { text: 'Something lame, who cares, whatever.', correct: false },
+	//         { text: 'Some kind of fish, right?', correct: false }
+	//     ]
+	// };
 	$scope.answered = false
 	$scope.answeredCorrectly
 	$scope.answerQuestion = function (answer) {
-		console.log(answer.text)
+
 		$scope.answered = true
+		
 		if (answer.correct) {
 			$scope.answeredCorrectly = true
 		}
+		
+		$scope.currentIndex++
+		
+		console.log($scope.current.question)
 
+		setTimeout(function(){
+			$scope.current = $scope.flashCards[$scope.currentIndex];
+			$scope.answered = false;
+			$scope.$digest();
+		}, 2000)
 	}
+})
+
+app.controller("MainController", function($scope){
+	$scope.flashCards = [
+    {
+        question: 'What is Angular?',
+        answers: [
+            { text: 'A front-end framework for great power!', correct: true },
+            { text: 'Something lame, who cares, whatever.', correct: false },
+            { text: 'Some kind of fish, right?', correct: false }
+        ]
+    },
+    {
+        question: 'What is a controller?',
+        answers: [
+            { text: 'Something that manages my front-end routes', correct: false },
+            { text: 'A function that allows me to manage a scope', correct: true },
+            { text: 'An Angular template', correct: false }
+        ]
+    },
+    {
+        question: 'What does {{ }} do?',
+        answers: [
+            { text: 'It runs some Javascript', correct: false },
+            { text: 'It looks for variables in HTML', correct: false },
+            { text: 'It runs an Angular expression that accesses my $scope', correct: true }
+        ]
+    }
+	];
+	$scope.currentIndex = 0
+	$scope.current = $scope.flashCards[$scope.currentIndex];
 })
