9990163dfbf27c8a2e5f9daee5b14c3a84913769
Samuel Leavin <sam.leavin@att.net>
2015-05-21 17:24:44 -0400

    updated

--- public/app.js
+++ public/app.js
@@ -1,14 +1,6 @@
 var app = angular.module("FlashCards", [])
 
 app.controller("FlashCardController", function ($scope, $timeout) {
-	// $scope.flashCard = {
-	//     question: 'What is Angular?',
-	//     answers: [
-	//         { text: 'A front-end framework for great power!', correct: true },
-	//         { text: 'Something lame, who cares, whatever.', correct: false },
-	//         { text: 'Some kind of fish, right?', correct: false }
-	//     ]
-	// };
 	$scope.answered = false
 	$scope.answeredCorrectly
 	$scope.answerQuestion = function (answer) {
